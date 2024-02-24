5d2ab2bed00346c2ff8a5dc07a1981701e188a1f
eddiespag-hetti <eddie_p89@hotmail.com>
2024-02-25 08:01:49 +1100

    Completed employeeTracker_db with tables and seeds.

--- db/schema.sql
+++ db/schema.sql
@@ -4,13 +4,26 @@ CREATE DATABASE employeeTracker_db;
 USE employeeTracker_db;
 
 CREATE TABLE department (
-
+id INT NOT NULL PRIMARY KEY,
+employee_name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
 
+
+CREATE TABLE roles (
+id INT NOT NULL PRIMARY KEY,
+title VARCHAR(30),
+salary DECIMAL
+department_id INT,
+FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-
+id INT PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT,
+manager_id INT,
+FOREIGN KEY (role_id) REFERENCES roles(id),
+FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
---
+++ seeds/seeds.sql
@@ -0,0 +1,38 @@
+INSERT INTO department (id, deptartment_name)
+VALUES 
+(1, 'Sales'),
+(2, 'Marketing'),
+(3, 'Human Resources'),
+(4, 'Finance'),
+(5, 'Information Technology'),
+(6, 'Customer Service'),
+(7, 'Research and Development'),
+(8, 'Operations'),
+(9, 'Legal'),
+(10, 'Administration');
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES
+(1, 'Sales Associate', 50000, 1),
+(2, 'Marketing Specialist', 60000, 2),
+(3, 'HR Manager', 70000, 3),
+(4, 'Financial Analyst', 65000, 4),
+(5, 'IT Specialist', 70000, 5),
+(6, 'Customer Service Representative', 45000, 6),
+(7, 'Research Scientist', 80000, 7),
+(8, 'Operations Manager', 75000, 8),
+(9, 'Legal Counsel', 90000, 9),
+(10, 'Administrative Assistant', 40000, 10);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) 
+VALUES
+(1, 'John', 'Doe', 1, NULL),
+(2, 'Jane', 'Smith', 2, NULL),
+(3, 'Michael', 'Johnson', 3, NULL),
+(4, 'Emily', 'Williams', 4, 3),
+(5, 'David', 'Brown', 5, 3),
+(6, 'Sarah', 'Davis', 6, 3),
+(7, 'Daniel', 'Miller', 7, 6),
+(8, 'Emma', 'Wilson', 8, 6),
+(9, 'Matthew', 'Taylor', 9, 6),
+(10, 'Olivia', 'Moore', 10, 6);
