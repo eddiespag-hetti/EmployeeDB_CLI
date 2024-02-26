0a39e36964af3bcb272e3fd6d9b270b0767fc030
eddiespag-hetti <eddie_p89@hotmail.com>
2024-02-26 11:59:32 +1100

    added dev deps wrote more functions relating to user selections from main menu

--- Develop/index.js
+++ Develop/index.js
@@ -1,24 +1,119 @@
-const inquirer = require('inquirer');
-
-
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+require("dotenv").config();
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    // MySQL password
+    password: "_3ddiE7689",
+    database: "employeeTracker_db",
+  },
+  console.log(`Connected to the courses_db database.`)
+);
 
 const promptUser = () => {
-    return inquirer.prompt([
-{
-    type: 'list',
-    name: 'view_options',
-    message: 'What would you like to do?',
-    choices: 
-    ['View all Employees', 'View all Employees by Department', 'View all Employees by Manager', 'Add Employee', 
-    'Remove Employee', 'Update Employee Role', 'Update Employee Manager', 'View all Departments', 'Add Department',
-'Remove Department', 'Quit']
-    
-}
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "view_options",
+        message: "What would you like to do?",
+        choices: [
+          "View all Employees",
+          "View all Departments",
+          "View all Roles",
+          "Add Department",
+          "Add Role",
+          "Add Employee",
+          "Update Employee Role",
+          "Quit",
+        ],
+      },
+    ])
 
-    ])}
+    .then((userChoice) => {
+      console.log(userChoice);
+      if (userChoice.view_options === "View all Employees") {
+        viewALLEmp();
+      }
 
-    const init = () => {
-        promptUser();
+      if (userChoice.view_options === "View all Departments") {
+        viewAllDepartments();
+      }
+
+      if (userChoice.view_options === "View all Roles") {
+        viewAllRoles();
+      }
+
+      if (userChoice.view_options === "Add Employee") {
+        addEmployee();
+      }
+    });
+};
+
+const init = () => {
+  promptUser();
+};
+
+// Functions for all user selections
+const viewALLEmp = () => {
+  db.query(`SELECT * FROM employee`, (err, result) => {
+    if (err) {
+      console.log(err);
     }
+    console.table(result);
+    promptUser();
+  });
+};
 
-    init();
+const viewAllDepartments = () => {
+  db.query(`SELECT * FROM department`, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.table(result);
+    promptUser();
+  });
+};
+
+const viewAllRoles = () => {
+  db.query(`SELECT * FROM role`, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.table(result);
+    promptUser();
+  });
+};
+
+const addEmployee = () => {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "firstname",
+      message: "What is their first name?",
+    },
+    {
+      type: "input",
+      name: "lastname",
+      message: "What is their last name?",
+    },
+    {
+        type: "input",
+        name: "what_role",
+        message: "What is their current role?",
+      },  
+      {
+        type: "input",
+        name: "which_manager",
+        message: "What is their current manager?",
+      },  
+    
+  ]).then((answers) => {
+    
+  })
+};
+
+// Initilise Function()
+init();
